0c8bf42856e7d44476efc72c579e9f62464efdf6
dtorredo <drickykanyiri@gmail.com>
2025-10-04 11:50:25 +0300

    clean up of the unnecessary files

--- prenatal-system/scripts/003_seed_data.sql
+++
@@ -1,9 +0,0 @@
--- This script will be used to seed initial data after user signup
--- Note: This is just a template. Actual data will be added after authentication is set up.
-
--- Example: Insert sample patients (will be added via the UI after authentication)
--- insert into public.patients (first_name, last_name, date_of_birth, phone, blood_type)
--- values 
---   ('Maria', 'Santos', '1995-03-15', '+1234567890', 'O+'),
---   ('Ana', 'Rodriguez', '1992-07-22', '+1234567891', 'A+'),
---   ('Sofia', 'Martinez', '1998-11-08', '+1234567892', 'B+');
